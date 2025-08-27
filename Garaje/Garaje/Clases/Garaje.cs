d6eca7a54198344ce490d5170178402b9ec0e2c1
Leslyantunez <Leslyantunez14@gmail.com>
2025-08-27 17:11:22 -0600

    Agregar archivos de proyecto.

---
+++ Garaje/Garaje/Clases/Garaje.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoGaraje.Clases
+{
+    internal class Garaje
+    {
+        public int NumeroGaraje { get; set; }
+        public string Descripcion { get; set; }
+        public int CapacidadEspacios { get; set; }
+        public int EspaciosOcupados { get; set; }
+
+
+        public Garaje(int numeroGaraje, string descripcion, int capacidadEspacios)
+        {
+            this.NumeroGaraje = numeroGaraje;
+            this.Descripcion = descripcion;
+            this.CapacidadEspacios = capacidadEspacios;
+            this.EspaciosOcupados = 0;
+        }
+
+        public Garaje()
+        { }
+        
+
+        public int getEspaciosDisponibles() 
+        {
+            return CapacidadEspacios - EspaciosOcupados;
+        }
+
+        public void getEspaciosOcupados(int EspaciosSolicitados)
+        {
+            if (EspaciosOcupados - EspaciosSolicitados <= CapacidadEspacios)
+            {
+                EspaciosOcupados = EspaciosOcupados + EspaciosSolicitados;
+                Console.WriteLine("Espacios ocupados actualizados " + EspaciosOcupados);
+            }
+            else
+            {
+                Console.WriteLine("No hay suficientes espacios disponibles");
+            }
+        }
+    }
+}
+
---
+++ Garaje/Garaje/Program.cs
@@ -0,0 +1,26 @@
+﻿using ProyectoGaraje.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoGaraje 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Garaje garaje1 = new Garaje(); 
+           
+            Console.WriteLine("Espacios disponibles: " + garaje1.getEspaciosDisponibles());
+            garaje1.getEspaciosOcupados(20);
+            Console.WriteLine("Espacios disponibles ahora: " + garaje1.getEspaciosDisponibles());
+            garaje1.getEspaciosOcupados(25);
+            Console.WriteLine("Espacios disponibles ahora: " + garaje1.getEspaciosDisponibles());
+
+            Console.ReadLine();
+
+        }
+    }
+}
